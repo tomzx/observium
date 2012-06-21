b97881909e8586b8a1a81f92853fdcda94a7e088
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2012-06-21 19:23:44 +0000

    fix ipmi polling/discovery + limits
    git-svn-id: http://www.observium.org/svn/observer/trunk@3302 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/ipmi.inc.php
+++ includes/polling/ipmi.inc.php
@@ -16,19 +16,31 @@ if ($ipmi['host'] = get_dev_attrib($device,'ipmi_hostname'))
     $remote = " -H " . $ipmi['host'] . " -L USER -U " . $ipmi['user'] . " -P " . $ipmi['password'];
   }
 
-  $results = external_exec($config['ipmitool'] . " -c " . $remote . " sdr 2>/dev/null");
+  $ipmi_start = utime();
 
+  $results = external_exec($config['ipmitool'] . $remote . " sensor 2>/dev/null");
+
+  $ipmi_end = utime(); $ipmi_time = round(($ipmi_end - $ipmi_start) * 1000);
+  
+  echo('(' . $ipmi_time . 'ms) ');
+          
   $index = 0;
 
   foreach (explode("\n",$results) as $row)
   {
     $index++;
-    list($descr,$current,$unit,$status) = explode(',',$row);
-    
+
+    # BB +1.1V IOH     | 1.089      | Volts      | ok    | na        | 1.027     | 1.054     | 1.146     | 1.177     | na
+    list($desc,$current,$unit,$state,$low_nonrecoverable,$low_limit,$low_warn,$high_warn,$high_limit,$high_nonrecoverable) = explode('|',$row);
+
     if (trim($current) != "na" && $config['ipmi_unit'][trim($unit)])
     {
-      discover_sensor($valid['sensor'], $config['ipmi_unit'][$unit], $device, '', $index, 'ipmi', $descr, '1', '1', NULL, NULL, NULL, NULL, $current, 'ipmi');
-      $ipmi_sensors[$config['ipmi_unit'][$unit]]['ipmi'][$index] = array('description' => $descr, 'current' => $current, 'index' => $index, 'unit' => $unit);
+      discover_sensor($valid['sensor'], $config['ipmi_unit'][trim($unit)], $device, '', $index, 'ipmi', trim($desc), '1', '1',
+        (trim($low_limit) == 'na' ? NULL : trim($low_limit)), (trim($low_warn) == 'na' ? NULL : trim($low_warn)),
+        (trim($high_warn) == 'na' ? NULL : trim($high_warn)), (trim($high_limit) == 'na' ? NULL : trim($high_limit)),
+        $current, 'ipmi');
+
+      $ipmi_sensors[$config['ipmi_unit'][$unit]]['ipmi'][$index] = array('description' => $desc, 'current' => $current, 'index' => $index, 'unit' => $unit);
     }
   }
 
