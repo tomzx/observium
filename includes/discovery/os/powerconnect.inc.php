8c9d9dca4c0e959ccdaa941d1d8239e832b87a5b
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2012-07-19 22:00:00 +0000

    fixup
    git-svn-id: http://www.observium.org/svn/observer/trunk@3341 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/powerconnect.inc.php
+++ includes/discovery/os/powerconnect.inc.php
@@ -4,7 +4,7 @@ if (!$os)
 {
   if (stristr($sysDescr, "PowerConnect ")) { $os = "powerconnect"; }
   else if (preg_match("/Dell.*Gigabit\ Ethernet/i",$sysDescr)) { $os = "powerconnect"; }
-  else if (preg_match("1\.3\.6\.1\.4\.1\.674\.10895\..*",$sysObjectId))
+  else if (preg_match("/1\.3\.6\.1\.4\.1\.674\.10895\..*/",$sysObjectId))
   {
     if (preg_match("/^24G Ethernet Switch$/",$sysDescr)) { $os = "powerconnect"; }
     else if (preg_match("/^48G Ethernet Switch$/",$sysDescr)) { $os = "powerconnect"; }
