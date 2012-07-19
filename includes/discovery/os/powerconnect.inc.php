26472b2b782f4fdcb77d7f5abe198119caefe5bd
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2012-07-19 21:58:50 +0000

    maybe recognize PowerConnect 5524/5548?
    git-svn-id: http://www.observium.org/svn/observer/trunk@3340 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/powerconnect.inc.php
+++ includes/discovery/os/powerconnect.inc.php
@@ -2,9 +2,13 @@
 
 if (!$os)
 {
-  #if (strstr($sysDescr, "Neyland 24T")) { $os = "powerconnect"; } /* Powerconnect 5324 */
   if (stristr($sysDescr, "PowerConnect ")) { $os = "powerconnect"; }
   else if (preg_match("/Dell.*Gigabit\ Ethernet/i",$sysDescr)) { $os = "powerconnect"; }
+  else if (preg_match("1\.3\.6\.1\.4\.1\.674\.10895\..*",$sysObjectId))
+  {
+    if (preg_match("/^24G Ethernet Switch$/",$sysDescr)) { $os = "powerconnect"; }
+    else if (preg_match("/^48G Ethernet Switch$/",$sysDescr)) { $os = "powerconnect"; }
+  }
 }
 
 ?>
