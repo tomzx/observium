97c3203399e94546513569efbef12caa3f15b93a
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2012-09-21 00:53:39 +0000

    packages prettification
    git-svn-id: http://www.observium.org/svn/observer/trunk@3385 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/packages.inc.php
+++ html/pages/device/packages.inc.php
@@ -1,22 +1,33 @@
 <?php
 
-echo('<table cellspacing="0" cellpadding="5" width="100%">');
+echo("<table class=\"table table-bordered table-striped\">\n");
+echo("  <thead>\n");
+echo("    <tr>\n");
+echo("      <th style=\"width: 300px;\">Package name</th>\n");
+echo("      <th>Version</th>\n");
+echo("      <th>Architecture</th>\n");
+echo("      <th>Type</th>\n");
+echo("      <th>Size</th>\n");
+echo("    </tr>\n");
+echo("  </thead>\n");
+echo("  <tbody>\n");
 
 $i=0;
 foreach (dbFetchRows("SELECT * FROM `packages` WHERE `device_id` = ? ORDER BY `name`", array($device['device_id'])) as $entry)
 {
-  echo('<tr class="list">');
-  echo('<td width=200><a href="'. generate_url($vars, array('name' => $entry['name'])).'">'.$entry['name'].'</a></td>');
+  echo("    <tr>\n");
+  echo("      <td><a href=\"". generate_url($vars, array('name' => $entry['name']))."\">".$entry['name']."</a></td>\n");
   if ($build != '') { $dbuild = '-'.$entry['build']; } else { $dbuild = ''; }
-  echo("<td>".$entry['version'].$dbuild."</td>");
-  echo("<td>".$entry['arch']."</td>");
-  echo("<td>".$entry['manager']."</td>");
-  echo("<td>".format_si($entry['size'])."</td>");
-  echo("</tr>");
+  echo("      <td>".$entry['version'].$dbuild."</td>\n");
+  echo("      <td>".$entry['arch']."</td>\n");
+  echo("      <td>".$entry['manager']."</td>\n");
+  echo("      <td>".format_si($entry['size'])."</td>\n");
+  echo("    </tr>\n");
 
   $i++;
 }
 
-echo("</table>");
+echo("  </tbody>\n");
+echo("</table>\n");
 
 ?>
--- html/pages/packages.inc.php
+++ html/pages/packages.inc.php
@@ -14,49 +14,65 @@ foreach ($vars as $var => $value)
   }
 }
 
-echo('<table cellspacing="0" cellpadding="5" width="100%">');
+echo("<table class=\"table table-bordered table-striped\" style=\"margin-top: 10px;\">\n");
+echo("  <thead>\n");
+echo("    <tr>\n");
+echo("      <th style=\"width: 300px;\">Package</th>\n");
+echo("      <th>Version</th>\n");
+echo("    </tr>\n");
+echo("  </thead>\n");
+echo("  <tbody>\n");
 
 foreach (dbFetchRows("SELECT * FROM `packages` WHERE 1 $where GROUP BY `name`", $param) as $entry)
 {
-  echo('<tr class="list">');
-  echo('<td width=200><a href="'. generate_url($vars, array('name' => $entry['name'])).'">'.$entry['name'].'</a></td>');
+  echo("    <tr>\n");
+  echo("      <td><a href=\"". generate_url($vars, array('name' => $entry['name']))."\">".$entry['name']."</a></td>\n");
+  echo("      <td>");
 
-  echo("<td>");
   foreach (dbFetchRows("SELECT * FROM `packages` WHERE `name` = ? ORDER BY version, build", array($entry['name'])) as $entry_v)
   {
     $entry['blah'][$entry_v['version']][$entry_v['build']][$entry_v['device_id']] = 1;
   }
 
+  $first = true;
   foreach ($entry['blah'] as $version => $bleu)
   {
 
-    $content = '<div style="width: 800px;">';
-
+    //$content = '<div style="width: 800px;">';
+    $content = "";
     foreach ($bleu as $build => $bloo)
     {
       if ($build) { $dbuild = '-' . $build; } else { $dbuild = ''; }
-      $content .= '<div style="background-color: #eeeeee; margin: 5px;"><span style="font-weight: bold; ">'.$version.$dbuild.'</span>';
+      //$content .= '<div style="background-color: #eeeeee; margin: 5px;"><span style="font-weight: bold; ">'.$version.$dbuild.'</span>';
+      $content .= $version.$dbuild;
       foreach ($bloo as $device_id => $no)
       {
         $this_device = device_by_id_cache($device_id);
-        $content .= '<span style="background-color: #f5f5f5; margin: 5px;">'.$this_device['hostname'].'</span> ';
-
+        //$content .= '<span style="background-color: #f5f5f5; margin: 5px;">'.$this_device['hostname'].'</span> ';
+        if (!empty($this_device['hostname'])) {
+          $content .= " - ".$this_device['hostname'];
+        }
       }
-      $content .= "</div>";
+      //$content .= "</div>";
     }
-    $content .= "</div>";
+    //$content .= "</div>";
     if (empty($vars['name']))
     {
-      echo("<span style='margin:5px;'>".overlib_link("", $version, $content,  NULL)."</span>");
+      if ($first) { $first = false; $middot = ""; } else { $middot = "&nbsp;&nbsp;&middot;&nbsp;&nbsp"; }
+      //echo("<span style='margin:5px;'>".overlib_link("", $version, $content,  NULL)."</span>");
+      echo($middot."<a href=\"javascript:;\" rel=\"tooltip\" title=\"".$content."\">".$version.$dbuild."</a>");
     } else {
       echo("$version $content");
     }
   }
 
-  echo("<td>");
-  echo("</tr>");
+  echo("      </td>\n");
+  echo("    </tr>\n");
 }
 
-echo("</table>");
+echo("  </tbody>\n");
+echo("</table>\n");
+echo("<script src=\"".$config['base_url']."js/bootstrap-tooltip.js\"></script>\n");
+echo("<script src=\"".$config['base_url']."js/billing.js\"></script>\n");
 
 ?>
