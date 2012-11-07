ac01f6725f7b86a09cf502c124be224a71512d18
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2012-11-07 15:04:21 +0000

    add serial number to Foundry/Brocade ironware devices, patch by Menno Kortekaas
    git-svn-id: http://www.observium.org/svn/observer/trunk@3405 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/ironware.inc.php
+++ includes/polling/os/ironware.inc.php
@@ -9,6 +9,9 @@ $hardware = rewrite_ironware_hardware($hardware);
 $version = trim(exec($config['snmpget'] . " -M ".$config['mibdir'] . " -O vqs -m FOUNDRY-SN-AGENT-MIB:FOUNDRY-SN-ROOT-MIB " . snmp_gen_auth($device) . " " .
 $device['hostname'].":".$device['port'] . " snAgBuildVer.0"));
 
+$serial = trim(exec($config['snmpget'] . " -M ".$config['mibdir'] . " -O vqs -m FOUNDRY-SN-AGENT-MIB:FOUNDRY-SN-ROOT-MIB " . snmp_gen_auth($device) . " " .
+$device['hostname'].":".$device['port'] . " snChasSerNum.0"), '"');
+
 $version = str_replace("V", "", $version);
 $version = str_replace("\"", "", $version);
 
