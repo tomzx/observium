210de95c76a7acbb81879427c3a9e25ae22d532b
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-01-09 02:52:58 +0000

    update delhost to bootstrap
    git-svn-id: http://www.observium.org/svn/observer/trunk@3511 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/delhost.inc.php
+++ html/pages/delhost.inc.php
@@ -1,4 +1,4 @@
-<h1>Delete Host</h1>
+<h2>Delete Device</h2>
 
 <?php
 
@@ -23,7 +23,7 @@ if (is_numeric($_REQUEST['id']))
     print_message("Are you sure you want to delete device " . $device['hostname'] . "?");
 ?>
 <br />
-<form name="form1" method="post" action="">
+<form name="form1" method="post" action="" class="form-horizontal" >
     <input type="hidden" name="id" value="<?php echo $_REQUEST['id'] ?>" />
     <input type="hidden" name="confirm" value="1" />
     <input type="submit" class="submit" name="Submit" value="Confirm host deletion" />
@@ -35,8 +35,25 @@ else
 {
 ?>
 
-<form name="form1" method="post" action="">
-  <p><select name="id">
+<form name="form1" method="post" action="" class="form-horizontal" >
+
+      <script type="text/javascript">
+        function showWarning(checked) {
+          $('#warning').toggle();
+          if (checked) {
+            $('#deleteBtn').removeAttr('disabled');
+          } else {
+            $('#deleteBtn').attr('disabled', 'disabled');
+          }
+        }
+      </script>
+
+  <fieldset>
+    <legend>Device selection</legend>
+    <div class="control-group">
+      <label class="control-label" for="id">Device</label>
+      <div class="controls">
+        <select name="id">
 
 <?php
 
@@ -46,10 +63,23 @@ foreach (dbFetchRows("SELECT * FROM `devices` ORDER BY `hostname`") as $data)
 }
 
 ?>
-    </select>
-    <input type="hidden" name="confirm" value="1" />
-    <input type="submit" class="submit" name="Submit" value="Delete Host" />
-</p>
+
+        </select>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="id">Confirm removal</label>
+      <div class="controls">
+        <input type="checkbox" name="confirm" value="confirm" onchange="javascript: showWarning(this.checked);">
+      </div>
+    </div>
+  </fieldset>
+
+  <div class="form-actions">
+    <button id="deleteBtn" type="submit" class="btn btn-danger" disabled="disabled"><i class="icon-remove icon-white"></i> Remove Device</button>
+  </div>
+
 </form>
 <?php
 }
