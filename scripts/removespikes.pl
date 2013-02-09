605b73b1365121c52d26770a74058a3727e4a804
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-02-09 21:39:14 +0000

    update removespokes perl script, from mike stupalov #203
    git-svn-id: http://www.observium.org/svn/observer/trunk@3569 61d68cd4-352d-0410-923a-c4978735b2b8

--- scripts/removespikes.pl
+++ scripts/removespikes.pl
@@ -1,34 +1,82 @@
 #!/usr/bin/perl -w
 #
-# matapicos v2.1 - Vins Vilaplana <vins at terra dot es)
+# matapicos v2.2 - Vins Vilaplana <vins at terra dot es)
 #
 # Translated by Humberto Rossetti Baptista <humberto at baptista dot name)
 # slight adjustments and code cleanup too :-)
 #
+# Changes:
+#  - 2007/02/27 - knobi@knobisoft.de - Various changes:
+#                    Add value-based chopping (-t value)
+#                    Add analysis only mode (-a)
+#                    Controll verbose/debug output using -d and -v
+#                    Add -h help option
+#                    Move to using the Getopt::Std package
+#                    Use "strict" mode
+#  - 2006/01/12 - vins@terra.es - "$!" takes other values in some perl interpreters (e.g. FreeBSD 4.11-R). Thanks to Atle Veka!
+#
 
-require "getopts.pl";
+use strict;
+use Getopt::Std;
+my %opt=();
+getopts("adhl:t:v",\%opt);
 
 my (@dump,%exp,@cols,@dbak,%tot,%por);
-my ($linea,$linbak,$lino,$cdo,$tresto,$tstamp,$a,$b,$cont);
+my ($linea,$linbak,$lino,$cdo,$tresto,$tstamp,$a,$b,$c,$cont);
 my $DEBUG = 0;
+my $ANALYZE = 0;
+my $VERBOSE = 0;
 
 # Limit % for cutting. Any peak representing less than this % will be cut
 my $LIMIT=0.6; # obs this is really %, so 0.6 means 0.6% (and not 0.006%!)
 
-&Getopts('l:');
+# Threshhold for cutting. Values above it will be chopped if "-t" is used
+my $THRESH=1.01e300; # Just set it to a very high default
 
-if ($#ARGV < 0) {
+# Flag to indicate whether we are doing "binning" or threshold based chopping
+my $BINNING=1;
+
+if ($opt{h} || ($#ARGV < 0)) {
    print "REMOVESPIKES: Remove spikes from RRDtool databases.\n\n";
    print "Usage:\n";
-   print "$0 [-l number]  name_of_database\n\n";
-   print "Where: number is the % limit of spikes to chop (default: $LIMIT)\n";
-   print "       and name_of_database ir the rrd file to be treated.\n";
+   print "$0 -d -a [-l number] [-t maxval] name_of_database\n\n";
+   print "Where:\n";
+   print "  -d enables debug messages\n";
+   print "  -a runs only the analysis phase of the script\n";
+   print "  -h prints this message\n";
+   print "  -l sets the % limit of spikes bin-based chopping (default: $LIMIT)\n";
+   print "  -t sets the value above which records are chopped. Disabled by default.\n";
+   print "     Enabling value-based chopping will disable bin-based chopping\n\n";
+   print "  -v Verbose mode. Shows some information\n";
+   print "  name_of_database is the rrd file to be treated.\n";
    exit;
 }
 
-if ($opt_l) { 
-   $LIMIT=$opt_l; 
-   print "Limit set to $LIMIT\n" if $DEBUG;
+if ($opt{d}) { 
+   $DEBUG = 1;
+   $VERBOSE = 1; 
+   print "Enabling DEBUG mode\n";
+}
+
+if ($opt{a}) { 
+   $ANALYZE = 1; 
+   print "Running in ANALYZE mode\n";
+}
+
+if ($opt{v}) { 
+   $VERBOSE = 1; 
+   print "Running in VERBOSE mode\n";
+}
+
+if ($opt{l}) { 
+   $LIMIT=$opt{l}; 
+   print "Limit for bin-based chopping set to $LIMIT\n" if $VERBOSE;
+}
+
+if ($opt{t}) { 
+   $THRESH=$opt{t}; 
+   $BINNING=0;
+   printf("Max Value set to %g, disabling bin-based chopping\n",$THRESH) if $VERBOSE;
 }
 
 # temporary filename:
@@ -37,7 +85,7 @@ my $tempfile="/tmp/matapicos.dump.$$";
 
 ###########################################################################
 # Dump the rrd database to the temporary file (as XML)
-`rrdtool dump $ARGV[0] > $tempfile`;
+system("rrdtool dump $ARGV[0] > $tempfile") == 0 or die "\n";
 
 # Scan the XML dump checking the variations and exponent deviations
 open(FICH,"<$tempfile") 
@@ -70,7 +118,7 @@ foreach $lino (sort keys %exp) {
   $por{$lino}=(100*$exp{$lino})/$tot{$a};
 }
 
-if ($DEBUG) { 
+if ($DEBUG || $ANALYZE) { 
    # Dumps percentages for debugging purposes
    print "--percentages--\n";
    foreach $lino (sort keys %exp) {
@@ -78,7 +126,8 @@ if ($DEBUG) {
      ($a)=$lino=~/^(\d+)\:/;
      print $tot{$a}." = ".$por{$lino}."%\n";
    }
-   print "\n\n\n";
+   print "---------------\n\n";
+   exit if $ANALYZE;
 }
 
 
@@ -102,10 +151,14 @@ while (<FICH>) {
     if ($linbak ne '') {
       for ($lino=0;$lino<=$#dump-1;$lino++) {   # for each DS:
         if ($dump[$lino]=~/\d\.\d+e.(\d+)\s/) { # grab number (and not a NaN)
+	  $c=$&;
           $a=$1*1;                              # and exponent
           $b=substr("0$lino",-2).":$1";         # calculate the max percentage of this DS
-          if ($por{$b}< $LIMIT) {               # if this line represents less than $LIMIT
-            $linea=$tstamp.$linbak;             # we dump it.
+          if (($BINNING &&                      #
+		($por{$b}< $LIMIT)) ||          # if this line represents less than $LIMIT
+	      (!$BINNING &&			#
+		($c > $THRESH))) {              # or the value is larger then $THRESH
+            $linea=$tstamp.$linbak;             # we dump it
             $cdo=1;
             $tresto=$linbak;
           }
@@ -114,7 +167,7 @@ while (<FICH>) {
     }
     $linbak=$tresto;
     if ($cdo==1) { 
-      print "Chopping peak at $tstamp\n";
+      print "Chopping peak at $tstamp\n" if $DEBUG;
       $cont++; }
   }
   
@@ -126,12 +179,12 @@ close FSAL;
 ###########################################################################
 # Cleanup and move new file to the place of original one
 # and original one gets backed up.
-if ($cont == 0) { print "No peaks found.!\n"; }
+if ($cont == 0 && $VERBOSE) { print "No peaks found.!\n"; }
 else {
   rename($ARGV[0],"$ARGV[0].old");
   $lino="rrdtool restore $tempfile.xml $ARGV[0]";
   system($lino);
-  die "$0: Unable to execute the rrdtool restore on $ARGV[0] - $! - $@\n"  if $!;
+  die "$0: Unable to execute the rrdtool restore on $ARGV[0] - $! - $@\n" if $? != 0;
 }
 
 # cleans up the files created
