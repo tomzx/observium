ae71f674461c9681f69c135a9c75f2186e176c89
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-02-11 08:12:04 +0000

    add usermenu page
    git-svn-id: http://www.observium.org/svn/observer/trunk@3576 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/usermenu.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+$links['add']      = generate_url(array('page' => 'adduser'));
+$links['edit']     = generate_url(array('page' => 'edituser'));
+$links['log']      = generate_url(array('page' => 'authlog'));
+$active['add']     = (($vars['page'] == "adduser") ? "active" : "");
+$active['edit']    = (($vars['page'] == "edituser") ? "active" : "");
+$active['log']     = (($vars['page'] == "authlog") ? "active" : "");
+$isUserlist        = (isset($vars['user_id']) ? true : false);
+
+echo("
+      <div class=\"navbar\" style=\"margin-top: 10px;\">
+        <div class=\"navbar-inner\">
+          <a class=\"brand\">Users:</a>
+          <ul class=\"nav\">
+            <li class=\"".$active['add']." first\"><a href=\"".$links['add']."\"><i class=\"icon-plus-sign\"></i> Add User</a></li>
+            <li class=\"".$active['edit']."\"><a href=\"".$links['edit']."\"><i class=\"icon-edit\"></i> Edit Users</a></li>
+            <li class=\"".$active['log']."\"><a href=\"".$links['log']."\"><i class=\"icon-calendar\"></i> Authlog</a></li>
+          </ul>");
+if ($isUserlist) {
+  echo("
+          <ul class=\"nav pull-right\">
+            <li class=\"first\"><a href=\"".$links['edit']."\"><i class=\"icon-chevron-left\"></i> <strong>Back to userlist</strong></a></li>
+          </ul>");
+}
+echo("
+        </div>
+      </div>");
+
+?>
