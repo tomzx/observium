fcfdd39cc777dc110575603092935158edeef9c1
mike <mike@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-03-08 22:30:58 +0000

    Correct detection pfSense and other FreeBSD based.
    git-svn-id: http://www.observium.org/svn/observer/trunk@3732 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/freebsd.inc.php
+++ includes/discovery/os/freebsd.inc.php
@@ -7,11 +7,13 @@ if (!$os)
   if (strstr($sysObjectId, "1.3.6.1.4.1.8072.3.2.8"))
   {
     $os = "freebsd";
-  } elseif (preg_match("/^FreeBSD/", $sysDescr)) {
+  } elseif (strstr($sysDescr, 'FreeBSD'))
+  {
     $os = "freebsd";
+  } elseif (strstr($sysDescr, "m0n0wall"))
+  {
+    $os = "monowall"; // m0n0wall, based on FreeBSD
   }
-
-  if (strstr($sysDescr, "m0n0wall")) { $os = "monowall"; }  // m0n0wall, based on FreeBSD
 }
 
 ?>
