04263d2e50c7b75e9ac87b74b93f6bedf2b29389
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-03-19 00:15:24 +0000

    only show current value, min and max are useless for page counter
    git-svn-id: http://www.observium.org/svn/observer/trunk@3774 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/pagecount.inc.php
+++ html/includes/graphs/device/pagecount.inc.php
@@ -8,12 +8,10 @@ $pagecount_rrd  = $config['rrd_dir'] . "/".$device['hostname']."/pagecount.rrd";
 
 if (file_exists($pagecount_rrd))
 {
-  $rrd_options .= " COMMENT:'                           Cur   Min  Max\\n'";
+  $rrd_options .= " COMMENT:'                                      Cur\\n'";
   $rrd_options .= " DEF:pagecount=".$pagecount_rrd.":pagecount:AVERAGE ";
-  $rrd_options .= " LINE1:pagecount#CC0000:'Pages printed    ' ";
-  $rrd_options .= " GPRINT:pagecount:LAST:%3.0lf ";
-  $rrd_options .= " GPRINT:pagecount:MIN:%3.0lf ";
-  $rrd_options .= " GPRINT:pagecount:MAX:%3.0lf\\\l ";
+  $rrd_options .= " LINE1:pagecount#CC0000:'Pages printed                   ' ";
+  $rrd_options .= " GPRINT:pagecount:LAST:%3.0lf\\\l";
 }
 
 ?>
