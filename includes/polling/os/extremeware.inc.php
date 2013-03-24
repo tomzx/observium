fc7d46e06ab8d89f984d1d22f63a7efe8dfa4cda
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-03-24 20:15:56 +0000

    remove old extreme mib
    git-svn-id: http://www.observium.org/svn/observer/trunk@3808 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/extremeware.inc.php
+++ includes/polling/os/extremeware.inc.php
@@ -1,13 +1,15 @@
 <?php
 
+### FIXME - this shit is ancient.
+
 echo("Doing Extreme: ");
 
-#BD6808 - Version 7.8.3 (Build 5) by Release_Master 03/15/10 14:27:35
-#Summit48 - Version 4.1.19 (Build 2) by Release_Master  Wed 08/09/2000  6:09p
-#Summit24e3 - Version 6.2e.1 (Build 20) by Release_Master_ABU Tue 05/27/2003 16:46:08
-#Summit48 - 1720 Garry - Version 4.1.19 (Build 2) by Release_Master  Wed 08/09/2000  6:09p
-#Summit48(Yonetan) - Version 4.1.19 (Build 2) by Release_Master  Wed 08/09/2000  6:09p
-#Alpine3808 - Version 7.2.0 (Build 33) by Release_Master 07/09/04 14:05:12
+// BD6808 - Version 7.8.3 (Build 5) by Release_Master 03/15/10 14:27:35
+// Summit48 - Version 4.1.19 (Build 2) by Release_Master  Wed 08/09/2000  6:09p
+// Summit24e3 - Version 6.2e.1 (Build 20) by Release_Master_ABU Tue 05/27/2003 16:46:08
+// Summit48 - 1720 Garry - Version 4.1.19 (Build 2) by Release_Master  Wed 08/09/2000  6:09p
+// Summit48(Yonetan) - Version 4.1.19 (Build 2) by Release_Master  Wed 08/09/2000  6:09p
+// Alpine3808 - Version 7.2.0 (Build 33) by Release_Master 07/09/04 14:05:12
 
 if (!strpos($poll_device['sysDescr'], "XOS"))
 {
@@ -24,8 +26,8 @@ if (!strpos($poll_device['sysDescr'], "XOS"))
 else
 {
 
-  #ExtremeXOS version 12.4.1.7 v1241b7 by release-manager on Sat Mar 13 02:36:57 EST 2010
-  #ExtremeWare XOS version 11.5.2.10 v1152b10 by release-manager on Thu Oct 26 09:53:04 PDT 2006
+  // ExtremeXOS version 12.4.1.7 v1241b7 by release-manager on Sat Mar 13 02:36:57 EST 2010
+  // ExtremeWare XOS version 11.5.2.10 v1152b10 by release-manager on Thu Oct 26 09:53:04 PDT 2006
 
   echo(" XOS \n");
   list($a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k,$l,$m) = explode(" ", str_replace("ExtremeWare XOS", "ExtremeXOS", $poll_device['sysDescr']));
