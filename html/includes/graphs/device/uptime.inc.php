5a4afa565a2430c4a31561c1c75ca9a3aa78a287
mike <mike@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-03-28 13:49:09 +0000

    small change.
    git-svn-id: http://www.observium.org/svn/observer/trunk@3832 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/uptime.inc.php
+++ html/includes/graphs/device/uptime.inc.php
@@ -8,10 +8,10 @@ $rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/uptime.rrd";
 
 $rrd_options .= " DEF:uptime=".$rrd_filename.":uptime:AVERAGE";
 $rrd_options .= " CDEF:cuptime=uptime,86400,/";
-$rrd_options .= " 'COMMENT:Days        Current  Minimum  Maximum  Average\\l'";
+$rrd_options .= " 'COMMENT:Days      Current  Minimum  Maximum  Average\\l'";
 $rrd_options .= " AREA:cuptime#EEEEEE:Uptime";
 $rrd_options .= " LINE1.25:cuptime#36393D:";
-$rrd_options .= " GPRINT:cuptime:LAST:%8.2lf  GPRINT:cuptime:MIN:%7.2lf";
+$rrd_options .= " GPRINT:cuptime:LAST:%6.2lf  GPRINT:cuptime:MIN:%7.2lf";
 $rrd_options .= " GPRINT:cuptime:MAX:%7.2lf  'GPRINT:cuptime:AVERAGE:%7.2lf\\l'";
 
 ?>
