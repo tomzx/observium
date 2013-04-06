1ecabfac03aaf14ea6e75f7123e089de9088df57
mike <mike@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-04-06 08:19:15 +0000

    Use php instead unix commands :P
    git-svn-id: http://www.observium.org/svn/observer/trunk@3866 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/icons.php
+++ html/icons.php
@@ -8,9 +8,17 @@
 <body>
 <?php
 
-$icons = "sweetie-arrow-branch-bgr sweetie-arrow-branch-byr sweetie-arrow-branch-gyr sweetie-arrow-down sweetie-arrow-incident-blue sweetie-arrow-incident-green sweetie-arrow-incident-red sweetie-arrow-left sweetie-arrow-right sweetie-arrow-up sweetie-asset-black sweetie-asset-blue sweetie-asset-green sweetie-asset-grey sweetie-asset-red sweetie-asset-tan sweetie-asset-teal sweetie-asset-white sweetie-asset-yellow sweetie-badge-circle-check sweetie-badge-circle-cross sweetie-badge-circle-direction-down sweetie-badge-circle-direction-left sweetie-badge-circle-direction-right sweetie-badge-circle-direction-up sweetie-badge-circle-minus sweetie-badge-circle-plus sweetie-badge-circle-power sweetie-badge-square-check sweetie-badge-square-cross sweetie-badge-square-direction-down sweetie-badge-square-direction-left sweetie-badge-square-direction-right sweetie-badge-square-direction-up sweetie-badge-square-minus sweetie-badge-square-plus sweetie-badge-square-power sweetie-check sweetie-circle-black sweetie-circle-blue sweetie-circle-glass sweetie-circle-green sweetie-circle-grey sweetie-circle-metal sweetie-circle-paper sweetie-circle-red sweetie-circle-tan sweetie-circle-teal sweetie-circle-white sweetie-circle-wood sweetie-circle-yellow sweetie-clipboard-audit sweetie-clipboard-comment sweetie-clipboard-eye sweetie-clipboard-fingerprint sweetie-clipboard-report-bar sweetie-clipboard-search sweetie-clipboard-warning sweetie-comment sweetie-comment-chat sweetie-comment-inactive sweetie-cross sweetie-dice-red sweetie-dice-white sweetie-email sweetie-email-minus sweetie-email-plus sweetie-heart sweetie-lightening sweetie-minus sweetie-note sweetie-note-star sweetie-note-todo sweetie-note-todo-list sweetie-page sweetie-page-forum sweetie-page-pencil sweetie-page-star sweetie-paper-arrow-blue sweetie-paper-arrow-green sweetie-paper-arrow-red sweetie-paper-arrow-teal sweetie-paper-arrow-yellow sweetie-paper-calculate-percent sweetie-paper-control sweetie-paper-excerpt-blue sweetie-paper-excerpt-green sweetie-paper-excerpt-red sweetie-paper-excerpt-teal sweetie-paper-excerpt-yellow sweetie-paper-gavel sweetie-paper-workplan sweetie-pencil sweetie-person sweetie-person-heart sweetie-person-minus sweetie-person-plus sweetie-person-profile sweetie-plus sweetie-search sweetie-settings sweetie-shield-blue-broken sweetie-square-black sweetie-square-blue sweetie-square-glass sweetie-square-green sweetie-square-grey sweetie-square-metal sweetie-square-paper sweetie-square-red sweetie-square-tan sweetie-square-teal sweetie-square-white sweetie-square-wood sweetie-square-yellow sweetie-star sweetie-star-inactive sweetie-warning sweetie-zoom-in sweetie-zoom-out ";
+/// FIXME. :D -- mike
+$icons = '';
+foreach (new SplFileObject('css/sweetie.css') as $line)
+{
+  if (preg_match('/\.(sweetie[\w\-]+)\s*{/', $line, $matches)) { $icons .= $matches[1] . ' '; }
+}
 
-$icons = shell_exec('/usr/bin/cat /home/observium/dev/html/css/sprites.css | /usr/bin/cut -f1 -d\'{\' | /usr/sed s/\.// | grep fugue\-[a-z] | /usr/bin/xargs');
+foreach (new SplFileObject('css/sprites.css') as $line)
+{
+  if (preg_match('/\.(fugue[\w\-]+)\s*{/', $line, $matches)) { $icons .= $matches[1] . ' '; }
+}
 
 foreach(explode(" ",$icons) as $icon)
 {
