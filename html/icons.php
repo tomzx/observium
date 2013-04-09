724d2ec9d615df5e1e400f4139aae9af6ab75b0a
mike <mike@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-04-09 14:30:44 +0000

    Show bootstrap icons.
    git-svn-id: http://www.observium.org/svn/observer/trunk@3892 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/icons.php
+++ html/icons.php
@@ -1,21 +1,23 @@
 <html>
 <head>
   <link rel="stylesheet" href="css/bootstrap.css" />
-  <link rel="stylesheet" href="css/sweetie.css" />
   <link rel="stylesheet" href="css/sprites.css" />
-
 </head>
 <body>
 <?php
 
-/// FIXME. :D -- mike
 $icons = '';
 foreach (new SplFileObject('css/sprites.css') as $line)
 {
   if (preg_match('/\.(oicon[\w\-]+)\s*{/', $line, $matches)) { $icons .= $matches[1] . ' '; }
 }
 
-foreach (explode(" ",$icons) as $icon)
+foreach (new SplFileObject('css/bootstrap.css') as $line)
+{
+  if (preg_match('/\.(icon[\w\-]+)\s*{/', $line, $matches)) { $icons .= $matches[1] . ' '; }
+}
+
+foreach (explode(" ", $icons) as $icon)
 {
   echo('<div style="margin: 2px; background-color: #f5f5f5; width: 260px; padding: 2px; float: left; height:16px; ">');
   echo('<a alt="'.$icon.'" class="'.$icon.'"><a> '.$icon);
