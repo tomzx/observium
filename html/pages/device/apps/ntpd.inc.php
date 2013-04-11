7b79df650496d5a47f806a6936ab2f33ab505153
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-04-11 16:42:09 +0000

    update ntp apps page
    git-svn-id: http://www.observium.org/svn/observer/trunk@3908 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/apps/ntpd.inc.php
+++ html/pages/device/apps/ntpd.inc.php
@@ -1,35 +1,53 @@
 <?php
 
+/**
+ * Observium
+ *
+ *   This files is part of Observium.
+ *
+ * @package    observium
+ * @subpackage applications
+ * @subpackage ntpd
+ * @author     Dennis de Houx <info@all-in-one.be>
+ * @copyright  (C) 2006 - 2013 Adam Armstrong
+ * @version    1.0.1
+ *
+ */
+
 global $config;
 
-$rrd_server = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-ntpd-server-".$app['app_id'].".rrd";
-$rrd_client = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-ntpd-client-".$app['app_id'].".rrd";
-$ntpd_type = (file_exists($rrd_server) ? "server" : "client");
-
+// Set variables
+$rrd_server      = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-ntpd-server-".$app['app_id'].".rrd";
+$rrd_client      = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-ntpd-client-".$app['app_id'].".rrd";
+$ntpd_type       = (file_exists($rrd_server) ? "server" : "client");
+$i               = 0;
 if ($ntpd_type == "server") {
-  $app_sections = array('server' => "System",
+  $app_sections  = array('server' => "System",
                         'buffer' => "Buffer",
                         'packets' => "Packets");
 } else {
-  $app_sections = array('client' => "System");
+  $app_sections  = array('client' => "System");
+}
+$navbar          = array();
+$navbar['brand'] = "NTPd";
+$navbar['class'] = "navbar-narrow";
+
+// Make navbar array
+foreach ($app_sections as $type=>$text) {
+  $i++;
+  if ($ntpd_type == "server") {
+    $vars['app_section']            = ((isset($vars['app_section'])) ? $vars['app_section'] : "server");
+  } else {
+    $vars['app_section']            = ((isset($vars['app_section'])) ? $vars['app_section'] : "client");
+  }
+  $navbar['options'][$i]['url']   = generate_url(array('page' => 'device', 'device' => $device['device_id'], 'tab' => 'apps', 'app' => 'ntpd', 'instance' => $app['app_id'], 'app_section' => $type));
+  $navbar['options'][$i]['text']  = nicecase($text);
+  $navbar['options'][$i]['class'] = (($vars['app_section'] == $type) ? "active" : "");
 }
 
-print_optionbar_start();
-echo('<span style="font-weight: bold;">'.$app["app_type"].'</span> &#187; ');
-unset($sep);
-foreach ($app_sections as $app_section => $app_section_text)
-{
-  echo($sep);
-  if (!$vars['app_section']) { $vars['app_section'] = $app_section; }
-  if ($vars['app_section'] == $app_section)
-  {
-    echo("<span class='pagemenu-selected'>");
-  }
-  echo(generate_link(ucfirst($app_section),$vars,array('app_section'=>$app_section)));
-  if ($vars['app_section'] == $app_section) { echo("</span>"); }
-  $sep = " | ";
-}
-print_optionbar_end();
+// Show navbar
+print_navbar($navbar);
+unset($navbar);
 
 $graphs['client'] = array('ntpd_stats'  => 'NTP Client - Statistics',
                           'ntpd_freq' => 'NTP Client - Frequency');
@@ -58,4 +76,3 @@ foreach ($graphs[$vars['app_section']] as $key => $text) {
 }
 
 ?>
-
