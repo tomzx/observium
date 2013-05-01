ce4a5d646a20051eaafee2e0c53f829c177752ab
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-05-01 21:01:49 +0000

    remove old broken code, sync temp and humidity sensor graphs
    git-svn-id: http://www.observium.org/svn/observer/trunk@3981 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/sensor/humidity.inc.php
+++ html/includes/graphs/sensor/humidity.inc.php
@@ -1,27 +1,23 @@
 <?php
 
 $scale_min = "25";
-$scale_max = "40";
+$scale_max = "80";
 
 include("includes/graphs/common.inc.php");
 
-$rrd_options .= " COMMENT:'                                 Last   Max\\n'";
+$rrd_options .= " COMMENT:'                          Min     Last   Max\\n'";
 
 $rrd_options .= " DEF:sensor=$rrd_filename:sensor:AVERAGE";
 $rrd_options .= " DEF:sensor_max=$rrd_filename:sensor:MAX";
 $rrd_options .= " DEF:sensor_min=$rrd_filename:sensor:MIN";
-$rrd_options .= " CDEF:sensorwarm=sensor_max,".$sensor['sensor_limit'].",GT,sensor,UNKN,IF";
-$rrd_options .= " CDEF:sensorcold=sensor_min,20,LT,sensor,UNKN,IF";
-$rrd_options .= " AREA:sensor_max#c5c5c5";
-$rrd_options .= " AREA:sensor_min#ffffffff";
+$rrd_options .= " CDEF:sensor_diff=sensor_max,sensor_min,-";
+$rrd_options .= " AREA:sensor_min";
+$rrd_options .= " AREA:sensor_diff#c5c5c5::STACK";
 
-#  $rrd_options .= " AREA:sensor#bbd392";
-#  $rrd_options .= " AREA:sensorwarm#FFCCCC";
-#  $rrd_options .= " AREA:sensorcold#CCCCFF";
-$rrd_options .= " LINE1:sensor#cc0000:'" . rrdtool_escape($sensor['sensor_descr'],28)."'";
-$rrd_options .= " LINE1:sensorwarm#660000";
-$rrd_options .= " GPRINT:sensor:LAST:%3.0lf%%";
-$rrd_options .= " GPRINT:sensor:MAX:%3.0lf%%\\\\l";
+$rrd_options .= " LINE1.5:sensor#cc0000:'" . rrdtool_escape($sensor['sensor_descr'],21)."'";
+$rrd_options .= " GPRINT:sensor_min:MIN:%4.1lf%%";
+$rrd_options .= " GPRINT:sensor:LAST:%4.1lf%%";
+$rrd_options .= " GPRINT:sensor_max:MAX:%4.1lf%%\\\\l";
 
 if (is_numeric($sensor['sensor_limit'])) $rrd_options .= " HRULE:".$sensor['sensor_limit']."#999999::dashes";
 if (is_numeric($sensor['sensor_limit_low'])) $rrd_options .= " HRULE:".$sensor['sensor_limit_low']."#999999::dashes";
