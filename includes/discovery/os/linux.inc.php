c69d251659e189381a1fe98566f327ca88e5b971
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-05-07 15:13:11 +0000

    fix stupid russian doll coding.
    git-svn-id: http://www.observium.org/svn/observer/trunk@3989 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/linux.inc.php
+++ includes/discovery/os/linux.inc.php
@@ -18,30 +18,16 @@ if (!$os)
     // Check for QNAP Systems TurboNAS
     $entPhysicalMfgName = snmp_get($device, "ENTITY-MIB::entPhysicalMfgName.1", "-Osqnv");
 
+    // Check for devices based on Linux
     if (strstr($sysObjectId, ".1.3.6.1.4.1.5528.100.20.10.2014")) { $os = "netbotz"; }
     elseif (strstr($sysDescr, "endian")) { $os = "endian"; }
     elseif (preg_match("/Cisco Small Business/", $sysDescr)) { $os = "ciscosmblinux"; }
     elseif (strpos($entPhysicalMfgName, "QNAP") !== FALSE) { $os = "qnap"; }
     elseif (strpos($sysObjectId, ".1.3.6.1.4.1.3375.2.1.3.4.") !== FALSE) { $os = "f5"; }
-    else
-    {
-      // Check for Synology DSM
-      $hrSystemInitialLoadParameters = trim(snmp_get($device, "hrSystemInitialLoadParameters.0", "-Osqnv"));
-      if (strpos($hrSystemInitialLoadParameters, "syno_hw_version") !== FALSE) { $os = "dsm"; }
-      else
-      {
-        // Check for Carel PCOweb
-        $roomTemp = trim(snmp_get($device,"roomTemp.0", "-OqvU", "CAREL-ug40cdz-MIB"));
-        if (is_numeric($roomTemp)) { $os = "pcoweb"; }
-        else
-        {
-          // Check for Ubiquiti AirOS
-          # IEEE802dot11-MIB::dot11manufacturerName.5 = STRING: Ubiquiti Networks, Inc.
-          $dot11manufacturerName = trim(snmp_get($device, "dot11manufacturerName.5", "-Osqnv", "IEEE802dot11-MIB"));
-          if (strpos($dot11manufacturerName, "Ubiquiti") !== FALSE) { $os = "airos"; }
-        }
-      }
-    }
+    elseif (is_numeric(trim(snmp_get($device,"roomTemp.0", "-OqvU", "CAREL-ug40cdz-MIB")))) { $os = "pcoweb"; }
+    elseif (strpos(trim(snmp_get($device, "hrSystemInitialLoadParameters.0", "-Osqnv")), "syno_hw_version") !== FALSE) { $os = "dsm"; }
+    elseif (strpos(trim(snmp_get($device, "dot11manufacturerName.5", "-Osqnv", "IEEE802dot11-MIB")), "Ubiquiti") !== FALSE) { $os = "airos"; }
+
   }
 }
 
