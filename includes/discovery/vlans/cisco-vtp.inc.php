73da716c5d8fe28943f0a69849b3464f049a9279
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-05-07 11:40:34 +0000

    update vlan name and mtu on changes
    git-svn-id: http://www.observium.org/svn/observer/trunk@3988 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/vlans/cisco-vtp.inc.php
+++ includes/discovery/vlans/cisco-vtp.inc.php
@@ -18,12 +18,28 @@ if ($device['os_group'] == "cisco")
       echo("VTP Domain  ".$vtpdomain_id." ".$vtpdomain['managementDomainName']." ");
       foreach ($vlans[$vtpdomain_id] as $vlan_id => $vlan)
       {
+        unset ($vlan_update);
+
+        if (is_array($vlans_db[$vtpdomain_id][$vlan_id]) && $vlans_db[$vtpdomain_id][$vlan_id]['vlan_name'] != $vlan['vtpVlanName'])
+        {
+          $vlan_update['vlan_name'] = $vlan['vtpVlanName'];
+        }
+
+        if (is_array($vlans_db[$vtpdomain_id][$vlan_id]) && $vlans_db[$vtpdomain_id][$vlan_id]['vlan_mtu'] != $vlan['vtpVlanMtu'])
+        {
+          $vlan_update['vlan_mtu'] = $vlan['vtpVlanMtu'];
+        }
+
         echo(" $vlan_id");
-        if (is_array($vlans_db[$vtpdomain_id][$vlan_id]))
+        if (is_array($vlan_update))
+        {
+          dbUpdate($vlan_update, 'vlans', 'vlan_id = ?', array($vlans_db[$vtpdomain_id][$vlan_id]['vlan_id']));
+          echo("U");
+        } elseif (is_array($vlans_db[$vtpdomain_id][$vlan_id]))
         {
           echo(".");
         } else {
-          dbInsert(array('device_id' => $device['device_id'], 'vlan_domain' => $vtpdomain_id, 'vlan_vlan' => $vlan_id, 'vlan_name' => $vlan['vtpVlanName'], 'vlan_type' => $vlan['vtpVlanType']), 'vlans');
+          dbInsert(array('device_id' => $device['device_id'], 'vlan_domain' => $vtpdomain_id, 'vlan_vlan' => $vlan_id, 'vlan_name' => $vlan['vtpVlanName'], 'vlan_mtu' => $vlan['vtpVlanMtu'], 'vlan_type' => $vlan['vtpVlanType']), 'vlans');
           echo("+");
         }
         $device['vlans'][$vtpdomain_id][$vlan_id] = $vlan_id;
