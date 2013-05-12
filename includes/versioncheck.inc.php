385797915acb11c4064b24f83889695fe8e7f160
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-05-12 14:43:22 +0000

    send usage of a few more modules
    git-svn-id: http://www.observium.org/svn/observer/trunk@3998 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/versioncheck.inc.php
+++ includes/versioncheck.inc.php
@@ -1,14 +1,35 @@
 <?php
 
 // Generate some statistics to send along with the version request.
+//
+// These stats are used to allow us to prioritise development resources
+// to target features and devices that are used the most.
 
-$stats['ports']        = dbFetchCell("SELECT count(*) FROM ports");
-$stats['devices']      = dbFetchCell("SELECT count(*) FROM devices");
-$stats['sensors']      = dbFetchCell("SELECT count(*) FROM sensors");
-$stats['services']     = dbFetchCell("SELECT count(*) FROM services");
-$stats['applications'] = dbFetchCell("SELECT count(*) FROM applications");
-$stats['bgp']          = dbFetchCell("SELECT count(*) FROM bgpPeers");
+// Overall Ports/Devices statistics
+$stats['ports']          = dbFetchCell("SELECT count(*) FROM ports");
+$stats['devices']        = dbFetchCell("SELECT count(*) FROM devices");
 
+// Per-feature statistics
+$stats['sensors']        = dbFetchCell("SELECT count(*) FROM sensors");
+$stats['services']       = dbFetchCell("SELECT count(*) FROM services");
+$stats['applications']   = dbFetchCell("SELECT count(*) FROM applications");
+$stats['bgp']            = dbFetchCell("SELECT count(*) FROM bgpPeers");
+$stats['ospf']           = dbFetchCell("SELECT count(*) FROM ospf_ports");
+$stats['eigrp']          = dbFetchCell("SELECT count(*) FROM eigrp_ports");
+$stats['ipsec_tunnels']  = dbFetchCell("SELECT count(*) FROM ipsec_tunnels");
+$stats['munin_plugins']  = dbFetchCell("SELECT count(*) FROM munin_plugins");
+$stats['pseudowires']    = dbFetchCell("SELECT count(*) FROM pseudowires");
+$stats['vrfs']           = dbFetchCell("SELECT count(*) FROM vrfs");
+$stats['vminfo']         = dbFetchCell("SELECT count(*) FROM vminfo");
+$stats['users']          = dbFetchCell("SELECT count(*) FROM users");
+
+// sysObjectID for Generic devices
+foreach (dbFetch("SELECT sysObjectID, COUNT( * ) as count FROM  `devices` WHERE `os` = 'generic' GROUP BY `sysObjectID`") as $gendata)
+{
+  $stats['generics'][$gendata['sysObjectID']] = $gendata['count'];
+}
+
+// Per-OS counts
 foreach (dbFetch("SELECT COUNT(*) AS count,os from devices group by `os`") as $dt_data)
 {
   $stats['devicetypes'][$dt_data['os']] = $dt_data['count'];
@@ -16,7 +37,7 @@ foreach (dbFetch("SELECT COUNT(*) AS count,os from devices group by `os`") as $d
 
 $stat_serial = base64_encode(serialize($stats));
 
-$url = "http://update.observium.org/latest.php?i=".$stats['ports']."&d=".$stats['devices']."&stats=".$stat_serial."&v=".$config['version'];
+$url = "http://update.observium.org/latest.php?i=".$stats['ports']."&d=".$stats['devices']."&stats=".$stat_serial."&v=".$config['version']."&gen_serial=".$gen_serial;
 $dataHandle = fopen($url, r);
 
 if ($dataHandle)
@@ -43,9 +64,9 @@ if ($dataHandle)
       {
         echo("Current Revision : $cur_revision\n");
 
-        if ($omnipotence > $cur_revision)
+        if ($revision > $cur_revision)
         {
-          echo("New Revision   : $omnipotence\n");
+          echo("New Revision   : $revision\n");
         }
 
 #        if ($omnipotence > $cur_omnipotence)
