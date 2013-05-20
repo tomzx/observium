a36d86686c43007f90dd28c792106b11b3a5cf4a
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-05-20 17:25:43 +0000

    don't need that one :)
    git-svn-id: http://www.observium.org/svn/observer/trunk@4040 61d68cd4-352d-0410-923a-c4978735b2b8

--- sql-schema/069.sql
+++ sql-schema/069.sql
@@ -1,4 +1,3 @@
-ALTER TABLE  `alert_conditions_global` ADD  `descr_regex` VARCHAR( 128 ) NULL DEFAULT NULL AFTER  `subtype`;
 TRUNCATE `ospf_areas`;
 TRUNCATE `ospf_instances`;
 TRUNCATE `ospf_nbrs`;
