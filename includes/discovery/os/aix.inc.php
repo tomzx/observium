aa6141aa39c11334615a86859e1c3839abe8f0db
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-05-29 05:43:33 +0000

    fix aix script
    git-svn-id: http://www.observium.org/svn/observer/trunk@4078 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/aix.inc.php
+++ includes/discovery/os/aix.inc.php
@@ -2,7 +2,7 @@
 
 if (!$os)
 {
-  if (strstr($sysObjectId, "1.3.6.1.4.1.2.3.1.2.1.1.3") ||
+  if (strstr($sysObjectId, "1.3.6.1.4.1.2.3.1.2.1.1.2") ||
       strstr($sysObjectId, "1.3.6.1.4.1.2.3.1.2.1.1.3") ) { $os = "aix"; }
 }
 
