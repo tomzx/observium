3512ef33004e29cd289835df0a9d2eb2a3905c61
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-05-31 00:25:41 +0000

    detect more equallogic
    git-svn-id: http://www.observium.org/svn/observer/trunk@4088 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/equallogic.inc.php
+++ includes/discovery/os/equallogic.inc.php
@@ -3,6 +3,7 @@
 if (!$os)
 {
   if (strstr($sysObjectId, ".1.3.6.1.4.1.12740.17.1")) { $os = "equallogic"; }
+  if (strstr($sysObjectId, ".1.3.6.1.4.1.12740.12.1.1.0")) { $os = "equallogic"; }
 }
 
 ?>
