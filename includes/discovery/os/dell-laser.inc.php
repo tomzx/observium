c2aa094018924e6cb73084a971e3807514bf82af
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-06-04 08:46:31 +0000

    add moar dell printers
    git-svn-id: http://www.observium.org/svn/observer/trunk@4112 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/dell-laser.inc.php
+++ includes/discovery/os/dell-laser.inc.php
@@ -2,9 +2,11 @@
 
 if (!$os)
 {
-  if (strstr($sysDescr, "Dell Color Laser")) { $os = "dell-laser"; }
+  if (strstr($sysObjectId, "1.3.6.1.4.1.674.10898.2.100.10")) { $os = "dell-laser"; }
+  elseif (strstr($sysDescr, "Dell Color Laser")) { $os = "dell-laser"; }
   elseif (strstr($sysDescr, "Dell Laser Printer")) { $os = "dell-laser"; }
   elseif (preg_match("/^Dell.*MFP/", $sysDescr)) { $os = "dell-laser"; }
+
 }
 
 ?>
