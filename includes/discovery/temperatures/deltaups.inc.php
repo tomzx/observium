a07e89e36d462a267c364f1efabff2c8cdce1e7d
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-06-14 16:32:15 +0000

    CODE, Y U NO LOOK RITE
    git-svn-id: http://www.observium.org/svn/observer/trunk@4147 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/temperatures/deltaups.inc.php
+++ includes/discovery/temperatures/deltaups.inc.php
@@ -3,16 +3,18 @@
 if ($device['os'] == "deltaups")
 {
   echo("DeltaUPS-MIB ");
+
   $dupsTemperature = array( 
-              array(  OID => "1.3.6.1.4.1.2254.2.4.10.1.0", 
-                            descr => "Environment",
-                            divisor => 1
-                        ),
-              array(  OID => "1.3.6.1.4.1.2254.2.4.7.9.0", 
-                            descr => "Battery",
-                            divisor => 1
-                        )
-             );
+    array('OID' => "1.3.6.1.4.1.2254.2.4.10.1.0", 
+          'descr' => "Environment",
+          'divisor' => 1
+         ),
+    array('OID' => "1.3.6.1.4.1.2254.2.4.7.9.0", 
+          'descr' => "Battery",
+          'divisor' => 1
+         )
+  );
+  
   foreach ($dupsTemperature as $eachArray => $eachValue)
   {
     // DeltaUPS does not have tables, so no need to walk, only need snmpget
@@ -21,10 +23,11 @@ if ($device['os'] == "deltaups")
     $preIndex = strstr($eachValue['OID'], '2254.2.4');
     // Format and strip index to only include everything after 2254.2.4
     $index = substr($preIndex, 9);
-      // Prevent NULL returned values from being added as sensors
-      if($current == "NULL")
-      {
-        discover_sensor($valid['sensor'], 'temperature', $device, $eachValue['OID'], $index, "DeltaUPS", $eachValue['descr'], $eachValue['divisor'], '1', NULL, NULL, NULL, NULL, $current);
-      }
+
+    // Prevent NULL returned values from being added as sensors
+    if($current == "NULL")
+    {
+      discover_sensor($valid['sensor'], 'temperature', $device, $eachValue['OID'], $index, "DeltaUPS", $eachValue['descr'], $eachValue['divisor'], '1', NULL, NULL, NULL, NULL, $current);
+    }
   }
 }
