551f2c207bf84f245d77662263d42a95f9f9d540
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-06-18 16:27:37 +0000

    netscaler service ttfb graph
    git-svn-id: http://www.observium.org/svn/observer/trunk@4158 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/device/netscalersvc_ttfb.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$colours='mixed';
+$nototal = 1;
+$ds  = "AvgSvrTTFB";
+
+include("includes/graphs/device/netscalersvc_common.inc.php");
+
+include("includes/graphs/generic_multi_line.inc.php");
+
+?>
