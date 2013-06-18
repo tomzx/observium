5020a8bb14cd929b257cfd14d50603f738f5fd59
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-06-18 16:27:58 +0000

    netscaler svc ttfb per-svc graph
    git-svn-id: http://www.observium.org/svn/observer/trunk@4159 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/netscalersvc/ttfb.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$ds  = "AvgSvrTTFB";
+
+$colour_area = "AA66AA";
+$colour_line = "330033";
+
+$colour_area_in = "cc88cc";
+
+$text  = "Client";
+
+$graph_max = 1;
+$unit_text = "ms to 1st byte";
+
+include("includes/graphs/generic_simplex.inc.php");
