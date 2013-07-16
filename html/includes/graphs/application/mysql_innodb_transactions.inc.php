817f59097668ede7d20183e021dd95ea8bdc1df8
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-07-16 13:43:37 +0000

    uppercasios es muy importante
    git-svn-id: http://www.observium.org/svn/observer/trunk@4193 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/application/mysql_innodb_transactions.inc.php
+++ html/includes/graphs/application/mysql_innodb_transactions.inc.php
@@ -26,7 +26,7 @@ if (is_file($rrd_filename))
 
 $colours   = "mixed";
 $nototal   = 1;
-$unit_text = "transactions";
+$unit_text = "Transactions";
 
 include("includes/graphs/generic_multi_simplex_seperated.inc.php");
 
--- html/includes/graphs/application/mysql_status.inc.php
+++ html/includes/graphs/application/mysql_status.inc.php
@@ -44,7 +44,7 @@ if (is_file($rrd_filename))
 
 $colours   = "mixed";
 $nototal   = 1;
-$unit_text = "activity";
+$unit_text = "Activity";
 
 include("includes/graphs/generic_multi_simplex_seperated.inc.php");
 
