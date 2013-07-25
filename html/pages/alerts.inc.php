9eb51d8fc24d66cd51b3e470d69f778111d39256
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-07-25 10:55:06 +0000

    fix button options on /alerts/
    git-svn-id: http://www.observium.org/svn/observer/trunk@4217 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/alerts.inc.php
+++ html/pages/alerts.inc.php
@@ -1,29 +1,5 @@
 <?php
 
-$navbar['class'] = "navbar-narrow";
-$navbar['brand'] = "Alert Types";
-
-$types = dbFetchRows("SELECT `entity_type` FROM `alert_tests` GROUP BY `entity_type`");
-
-foreach ($types as $thing)
-{
-  if (!$vars['entity_type']) { $vars['entity_type'] = $thing['entity_type']; }
-  if ($vars['entity_type'] == $thing['entity_type']) { $navbar['options'][$thing['entity_type']]['class'] = "active"; }
-
-  $navbar['options'][$thing['entity_type']]['url'] = generate_url(array('page' => 'alerts', 'entity_type' => $thing['entity_type']));
-  $navbar['options'][$thing['entity_type']]['text'] = htmlspecialchars(nicecase($thing['entity_type']));
-}
-
-$navbar['options_right']['alarmed']['url']  = generate_url(array('page' => 'alerts', 'alerted' => '1'));
-$navbar['options_right']['alarmed']['text'] = 'Alarmed Only';
-if ($vars['alerted'] == '1') { $navbar['options_right']['alarmed']['class'] = 'active'; }
-
-$navbar['options_right']['update']['url']  = generate_url(array('page' => 'alerts', 'action'=>'update'));
-$navbar['options_right']['update']['text'] = 'Regenerate';
-if ($vars['action'] == 'update') { $navbar['options_right']['update']['class'] = 'active'; }
-
-print_navbar($navbar);
-
 // Run actions
 
 if($vars['action'] == 'update')
@@ -32,7 +8,47 @@ if($vars['action'] == 'update')
   {
     update_device_alert_table($device);
   }
+
+  unset($vars['action']);
+
 }
+
+$navbar['class'] = "navbar-narrow";
+$navbar['brand'] = "Alert Types";
+
+$types = dbFetchRows("SELECT `entity_type` FROM `alert_tests` GROUP BY `entity_type`");
+
+$navbar['options']['all']['url'] = generate_url(array('page' => 'alerts', 'entity_type' => 'all'));
+$navbar['options']['all']['text'] = htmlspecialchars(nicecase('all'));
+if ($vars['entity_type'] == 'all') {
+  $navbar['options']['all']['class'] = "active";
+  $navbar['options']['all']['url'] = generate_url(array('page' => 'alerts', 'entity_type' => NULL));
+}
+
+foreach ($types as $thing)
+{
+  if (!$vars['entity_type']) { $vars['entity_type'] = $thing['entity_type']; }
+  if ($vars['entity_type'] == $thing['entity_type'])
+  {
+    $navbar['options'][$thing['entity_type']]['class'] = "active";
+    $navbar['options'][$thing['entity_type']]['url'] = generate_url($vars, array('page' => 'alerts', 'entity_type' => NULL));
+  } else {
+    $navbar['options'][$thing['entity_type']]['url'] = generate_url($vars, array('page' => 'alerts', 'entity_type' => $thing['entity_type']));
+  }
+  $navbar['options'][$thing['entity_type']]['text'] = htmlspecialchars(nicecase($thing['entity_type']));
+}
+
+$navbar['options_right']['alarmed']['url']  = generate_url($vars, array('page' => 'alerts', 'alerted' => '1'));
+$navbar['options_right']['alarmed']['text'] = 'Alarmed Only';
+if ($vars['alerted'] == '1') { $navbar['options_right']['alarmed']['class'] = 'active';
+$navbar['options_right']['alarmed']['url']  = generate_url($vars, array('page' => 'alerts', 'alerted' => NULL));}
+
+
+$navbar['options_right']['update']['url']  = generate_url($vars, array('page' => 'alerts', 'action'=>'update'));
+$navbar['options_right']['update']['text'] = 'Regenerate';
+if ($vars['action'] == 'update') { $navbar['options_right']['update']['class'] = 'active'; }
+
+print_navbar($navbar);
 
 $alert_rules = cache_alert_rules();
 
@@ -59,7 +75,7 @@ echo('<table class="table table-condensed table-bordered table-striped table-rou
   {
     $sql .= " AND `alert_status` = '0'";
   }
-  if(isset($vars['entity_type']))
+  if(isset($vars['entity_type']) && $vars['entity_type'] != 'all')
   {
     $sql .= " AND `entity_type` = ?";
     $args[] = $vars['entity_type'];
