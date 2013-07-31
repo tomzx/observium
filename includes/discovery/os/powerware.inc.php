91f495ab1c5aabb7d0d6373cad59a183f1df9501
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-07-31 14:23:22 +0000

    detect more powerware
    git-svn-id: http://www.observium.org/svn/observer/trunk@4230 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/powerware.inc.php
+++ includes/discovery/os/powerware.inc.php
@@ -2,7 +2,7 @@
 
 if (!$os)
 {
-  if (strstr($sysObjectId, ".1.3.6.1.4.1.534")) { $os = "powerware"; }
+  if (strstr($sysObjectId, ".1.3.6.1.4.1.534") || strstr($sysObjectId, ".1.3.6.1.4.1.705.1")) { $os = "powerware"; }
 }
 
 ?>
