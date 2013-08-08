b157bc3734f22d5f459ce9abdfae9084bc5aaed5
adama <adama@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-08-08 18:43:57 +0000

    fix h3c detection
    git-svn-id: http://www.observium.org/svn/observer/trunk@4269 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/hp.inc.php
+++ includes/discovery/os/hp.inc.php
@@ -6,9 +6,8 @@ if (!$os)
   // Legacy Procurve, H3C and new HP H3C detection. This should be all that's needed.
 
   if (strstr($sysObjectId, ".1.3.6.1.4.1.11.2.3.7.11")) { $os = "procurve"; }
-  if (strstr($sysObjectId, ".1.3.6.1.4.1.2011"))        { $os = "h3c"; }
-
-  if (strstr($sysObjectId, ".1.3.6.1.4.1.25506.1"))       { $os = "hh3c"; }
+  if (strstr($sysObjectId, ".1.3.6.1.4.1.2011.10"))     { $os = "h3c"; }
+  if (strstr($sysObjectId, ".1.3.6.1.4.1.25506"))       { $os = "hh3c"; }
 
 }
 
