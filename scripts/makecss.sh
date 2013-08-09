0c90f0141f27bb4ab10a5afb813d76604a16a45f
mike <mike@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-08-09 14:00:28 +0000

    Replace bootstrap sprites with font Awesome v3.2.1.
    git-svn-id: http://www.observium.org/svn/observer/trunk@4275 61d68cd4-352d-0410-923a-c4978735b2b8

--- scripts/makecss.sh
+++ scripts/makecss.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 lessc -x html/css/bootstrap/less/bootstrap.less > html/css/bootstrap.css
-lessc -x html/css/bootstrap/less/bootstrap-email.less > html/css/bootstrap-email.css
+#lessc -x html/css/bootstrap/less/bootstrap-email.less > html/css/bootstrap-email.css
 
