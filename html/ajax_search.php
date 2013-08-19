d50e8e986308a6fa5d6341a0d909719cae314d74
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-08-19 15:06:08 +0000

    show message when search returns no results; reindent file
    git-svn-id: http://www.observium.org/svn/observer/trunk@4303 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/ajax_search.php
+++ html/ajax_search.php
@@ -45,10 +45,13 @@ if (isset($_POST['queryString']) || isset($_GET['queryString']))
   // Is the string length greater than 0?
   if (strlen($queryString) >0)
   {
+    $found = 0;
+  
     /// SEARCH DEVICES
     $results = dbFetchRows("SELECT * FROM `devices` WHERE `hostname` LIKE '%" . $queryString . "%' OR `location` LIKE '%" . $queryString . "%' ORDER BY hostname LIMIT 8");
     if (count($results))
     {
+      $found = 1;
       echo '<li class="nav-header">Devices found: '.count($results).'</li>';
 
       foreach ($results as $result)
@@ -70,81 +73,88 @@ if (isset($_POST['queryString']) || isset($_GET['queryString']))
                     <small>'.$result['hardware'].' | '.$config['os'][$result['os']]['text'].' '. $result['version'] .'
                     <br /> '.highlight_search($result['location']).' | '.$num_ports.'ports</small></dd>
                 </dl>');
-        }
-
-        echo("</a></li>");
       }
 
-      /// SEARCH PORTS
-      $results = dbFetchRows("SELECT * FROM `ports` ".
-                             "LEFT JOIN `devices` ON  `ports`.`device_id` =  `devices`.`device_id` ".
-                             "WHERE `ifAlias` LIKE '%" . $queryString . "%' OR `ifDescr` LIKE '%" . $queryString . "%' ORDER BY ifDescr LIMIT 8");
+      echo("</a></li>");
+    }
 
-      if (count($results))
+    /// SEARCH PORTS
+    $results = dbFetchRows("SELECT * FROM `ports` ".
+                           "LEFT JOIN `devices` ON  `ports`.`device_id` =  `devices`.`device_id` ".
+                           "WHERE `ifAlias` LIKE '%" . $queryString . "%' OR `ifDescr` LIKE '%" . $queryString . "%' ORDER BY ifDescr LIMIT 8");
+
+    if (count($results))
+    {
+      $found = 1;
+      echo '<li class="nav-header">Ports found: '.count($results).'</li>';
+
+      foreach ($results as $result)
       {
-        echo '<li class="nav-header">Ports found: '.count($results).'</li>';
+        echo('<li class="divider" style="margin: 0px;"></li>');
+        echo('<li>');
+        echo '<a href="'.generate_port_url($result).'">';
+        $name = $result['ifDescr'];
+        if (strlen($name) > 35) { $name = substr($name, 0, 35) . "..."; }
+        $description = $result['ifAlias'];
+        if (strlen($description) > 80) { $description = substr($description, 0, 80) . "..."; }
 
-        foreach ($results as $result)
-        {
-          echo('<li class="divider" style="margin: 0px;"></li>');
-          echo('<li>');
-          echo '<a href="'.generate_port_url($result).'">';
-          $name = $result['ifDescr'];
-          if (strlen($name) > 35) { $name = substr($name, 0, 35) . "..."; }
-          $description = $result['ifAlias'];
-          if (strlen($description) > 80) { $description = substr($description, 0, 80) . "..."; }
+         /// FIXME : THIS SUCKS
+         if ($result['ifAdminStatus'] == "down") { $icon = "search-port-disabled"; $tab_colour = '#009900'; // FIXME: Why green for ignore? Also see humanize_device()
+         } elseif ($result['ifAdminStatus'] == "up" && $result['ifOperStatus']== "down") { $icon = "search-port-down"; $tab_colour = '#AAAAAA';
+         } elseif ($result['ifAdminStatus'] == "up" && $result['ifOperStatus']== "lowerLayerDown") { $icon = "search-port-down"; $tab_colour = '#AAAAAA';
+         } elseif ($result['ifAdminStatus'] == "up" && $result['ifOperStatus']== "up") { $icon = "search-port-up"; $tab_colour = '#194B7F'; // FIXME: This colour pulled from functions.inc.php humanize_device, maybe set it centrally in definitions?
+         }
 
-          /// FIXME : THIS SUCKS
-          if ($result['ifAdminStatus'] == "down") { $icon = "search-port-disabled"; $tab_colour = '#009900'; // FIXME: Why green for ignore? Also see humanize_device()
-          } elseif ($result['ifAdminStatus'] == "up" && $result['ifOperStatus']== "down") { $icon = "search-port-down"; $tab_colour = '#AAAAAA';
-          } elseif ($result['ifAdminStatus'] == "up" && $result['ifOperStatus']== "lowerLayerDown") { $icon = "search-port-down"; $tab_colour = '#AAAAAA';
-          } elseif ($result['ifAdminStatus'] == "up" && $result['ifOperStatus']== "up") { $icon = "search-port-up"; $tab_colour = '#194B7F'; // FIXME: This colour pulled from functions.inc.php humanize_device, maybe set it centrally in definitions?
-}
+         echo('<dl style="border-left: 10px solid '.$tab_colour.'; " class="dl-horizontal dl-search">
+                <dt style="padding-left: 10px; text-align: center;">
+                  <img src="images/'.$icon.'.png" /></dt>
+                <dd><h5>'.highlight_search($name).'</h5>
+                     <small>'.$result['hostname'].'<br />'.highlight_search($description).'</small></dd>
+                </dl>');
 
-          echo('<dl style="border-left: 10px solid '.$tab_colour.'; " class="dl-horizontal dl-search">
-                  <dt style="padding-left: 10px; text-align: center;">
-                    <img src="images/'.$icon.'.png" /></dt>
-                  <dd><h5>'.highlight_search($name).'</h5>
-                       <small>'.$result['hostname'].'<br />'.highlight_search($description).'</small></dd>
-                  </dl>');
+       }
 
-        }
+       echo("</a></li>");
+     }
 
-        echo("</a></li>");
-      }
+    /// SEARCH SENSORS
+    $results = dbFetchRows("SELECT * FROM `sensors` ".
+                           "LEFT JOIN `devices` ON  `sensors`.`device_id` =  `devices`.`device_id` ".
+                           "WHERE `sensor_descr` LIKE '%" . $queryString . "%' ORDER BY sensor_descr LIMIT 8");
 
-      /// SEARCH SENSORS
-      $results = dbFetchRows("SELECT * FROM `sensors` ".
-                             "LEFT JOIN `devices` ON  `sensors`.`device_id` =  `devices`.`device_id` ".
-                             "WHERE `sensor_descr` LIKE '%" . $queryString . "%' ORDER BY sensor_descr LIMIT 8");
+    if (count($results))
+    {
+      $found = 1;
+      echo '<li class="nav-header">Sensors found: '.count($results).'</li>';
 
-      if (count($results))
+      foreach ($results as $result)
       {
-        echo '<li class="nav-header">Sensors found: '.count($results).'</li>';
+        echo('<li class="divider" style="margin: 0px;"></li>');
+        echo('<li>');
+        echo '<a href="graphs/type=sensor_'  . $result['sensor_class'] . '/id=' . $result['sensor_id'] . '/">';
+        $name = $result['sensor_descr'];
+        if (strlen($name) > 35) { $name = substr($name, 0, 35) . "..."; }
 
-        foreach ($results as $result)
-        {
-          echo('<li class="divider" style="margin: 0px;"></li>');
-          echo('<li>');
-          echo '<a href="graphs/type=sensor_'  . $result['sensor_class'] . '/id=' . $result['sensor_id'] . '/">';
-          $name = $result['sensor_descr'];
-          if (strlen($name) > 35) { $name = substr($name, 0, 35) . "..."; }
+        /// FIXME: once we have alerting, colour this to the sensor's status
+        $tab_colour = '#194B7F'; // FIXME: This colour pulled from functions.inc.php humanize_device, maybe set it centrally in definitions?
 
-          /// FIXME: once we have alerting, colour this to the sensor's status
-          $tab_colour = '#194B7F'; // FIXME: This colour pulled from functions.inc.php humanize_device, maybe set it centrally in definitions?
-
-          echo('<dl style="border-left: 10px solid '.$tab_colour.'; " class="dl-horizontal dl-search">
-                  <dt style="padding-left: 10px; text-align: center;">
-                    <i class="'.$config['sensor_types'][$result['sensor_class']]['icon'].'"></i></dt>
-                  <dd><h5>'.highlight_search($name).'</h5>
-                       <small>'.$result['hostname'].'<br />
-                       '.$result['location'] . ' | ' .ucfirst($result['sensor_class']).' sensor</small></dd>
-                  </dl>');
-        }
-
-        echo("</a></li>");
+        echo('<dl style="border-left: 10px solid '.$tab_colour.'; " class="dl-horizontal dl-search">
+                <dt style="padding-left: 10px; text-align: center;">
+                  <i class="'.$config['sensor_types'][$result['sensor_class']]['icon'].'"></i></dt>
+                <dd><h5>'.highlight_search($name).'</h5>
+                     <small>'.$result['hostname'].'<br />
+                     '.$result['location'] . ' | ' .ucfirst($result['sensor_class']).' sensor</small></dd>
+                </dl>');
       }
-   } // There is a queryString.
+
+      echo("</a></li>");
+    }
+
+    if (!$found)
+    {
+      echo '<li class="nav-header">No search results.</li>';
+    }      
+  } // There is a queryString.
 } else {
    echo 'There should be no direct access to this script!';
 }
