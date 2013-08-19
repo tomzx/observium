ed2e13044e1b87e32c0f9dc30129a15514008772
mike <mike@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-08-19 12:08:08 +0000

    Moved to the right place.
    git-svn-id: http://www.observium.org/svn/observer/trunk@4301 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/graph.inc.php
+++ html/includes/graphs/graph.inc.php
@@ -23,34 +23,6 @@ if (is_numeric($vars['device']))
   $device = device_by_name($vars['device']);
 }
 
-if ($type == 'port' && !is_numeric($vars['id']))
-{
-  if (is_numeric($device['device_id']))
-  {
-    if (is_numeric($vars['ifindex']))
-    {
-      // Get port by ifIndex
-      $port = get_port_by_index_cache($device['device_id'], $vars['ifindex']);
-      if ($port) { $vars['id'] = $port['port_id']; }
-    } elseif (!empty($vars['ifdescr']))
-    {
-      // Get port by ifDescr
-      $port_id = get_port_id_by_ifDescr($device['device_id'], $vars['ifdescr']);
-      if ($port_id) { $vars['id'] = $port_id; }
-    } elseif (!empty($vars['ifalias']))
-    {
-      // Get port by ifAlias
-      $port_id = get_port_id_by_ifAlias($device['device_id'], $vars['ifalias']);
-      if ($port_id) { $vars['id'] = $port_id; }
-    }
-  } elseif (!empty($vars['circuit']))
-  {
-    // Get port by circuit id
-    $port_id = get_port_id_by_customer(array('circuit' => $vars['circuit']));
-    if ($port_id) { $vars['id'] = $port_id; }
-  }
-}
-
 // FIXME -- remove these
 
 // $from, $to - unixtime (or rrdgraph time interval, i.e. '-1d', '-6w')
--- html/includes/graphs/port/auth.inc.php
+++ html/includes/graphs/port/auth.inc.php
@@ -1,9 +1,38 @@
 <?php
 
-/// If we've been given a 'port' variable, try to work out the id from this, as it's the ifAlias
-if (is_array($device) && empty($vars['id']) && !empty($vars['port']))
+// Get port ID by ifIndex/ifDescr/ifAlias or customer circuit
+// variables: ifindex, ifdescr (or port), ifalias, circuit
+if (!is_numeric($vars['id']))
 {
-  $vars['id'] = get_port_id_by_ifDescr($device['device_id'], $vars['port']);
+  if (is_numeric($device['device_id']))
+  {
+    if (is_numeric($vars['ifindex']))
+    {
+      // Get port by ifIndex
+      $port = get_port_by_index_cache($device['device_id'], $vars['ifindex']);
+      if ($port) { $vars['id'] = $port['port_id']; }
+    } elseif (!empty($vars['ifdescr']))
+    {
+      // Get port by ifDescr
+      $port_id = get_port_id_by_ifDescr($device['device_id'], $vars['ifdescr']);
+      if ($port_id) { $vars['id'] = $port_id; }
+    } elseif (!empty($vars['port']))
+    {
+      // Get port by ifDescr (backward compatibility)
+      $port_id = get_port_id_by_ifDescr($device['device_id'], $vars['port']);
+      if ($port_id) { $vars['id'] = $port_id; }
+    } elseif (!empty($vars['ifalias']))
+    {
+      // Get port by ifAlias
+      $port_id = get_port_id_by_ifAlias($device['device_id'], $vars['ifalias']);
+      if ($port_id) { $vars['id'] = $port_id; }
+    }
+  } elseif (!empty($vars['circuit']))
+  {
+    // Get port by circuit id
+    $port_id = get_port_id_by_customer(array('circuit' => $vars['circuit']));
+    if ($port_id) { $vars['id'] = $port_id; }
+  }
 }
 
 if (is_numeric($vars['id']) && ($auth || port_permitted($vars['id'])))
@@ -13,7 +42,7 @@ if (is_numeric($vars['id']) && ($auth || port_permitted($vars['id'])))
   $title  = generate_device_link($device);
   $title .= " :: Port  <b>".generate_port_link($port) ."</b>";
 
-  $graph_title = shorthost($device['hostname']) . " ::" . strtolower(makeshortif($port['ifDescr']))."";
+  $graph_title = shorthost($device['hostname']) . " :: " . strtolower(makeshortif($port['ifDescr']))."";
   $auth   = TRUE;
   $rrd_filename = get_port_rrdfilename($device, $port);
 }
