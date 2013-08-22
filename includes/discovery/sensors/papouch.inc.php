5c5104efa0655f9b6b2554a02ffc14faecc26152
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-08-22 08:39:22 +0000

    consolidate papouch sensor discovery
    git-svn-id: http://www.observium.org/svn/observer/trunk@4321 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/sensors/papouch.inc.php
+++ includes/discovery/sensors/papouch.inc.php
@@ -58,6 +58,25 @@ if ($device['os'] == "papouch")
     discover_sensor($valid['sensor'], 'temperature', $device, $temperature_oid, "3", 'papouch-th2e', "Dew Point" , '10', '1', $low_limit, NULL, NULL, $high_limit, $temperature);
   }
 
+  $humidity = snmp_get($device, "SNMPv2-SMI::enterprises.18248.20.1.2.1.1.2.1", "-Oqv") / 10;
+
+  if (is_numeric($humidity) && $humidity > "0")
+  {
+    if (snmp_get($device, "SNMPv2-SMI::enterprises.18248.20.1.2.1.1.1.2", "-Oqv"))
+    {
+      $low_limit = snmp_get($device, "SNMPv2-SMI::enterprises.18248.20.1.3.1.1.2.2", "-Oqv") / 10;
+      $high_limit = NULL; # The MIB is invalid and I can't find the max value in snmpwalk :[ *sigh*
+      # Hysteresis parameter value is in SNMPv2-SMI::enterprises.18248.20.1.3.1.1.3.2 = INTEGER: 100
+    }
+    else
+    {
+      $low_limit = NULL;
+      $high_limit = NULL;
+    }
+
+    $humidity_oid = ".1.3.6.1.4.1.18248.20.1.2.1.1.2.2";
+    discover_sensor($valid['sensor'], 'humidity', $device, $humidity_oid, "1", 'papouch-th2e', "Humidity" , '10', '1', $low_limit, NULL, NULL, $high_limit, $humidity);
+  }
 }
 
 ?>
