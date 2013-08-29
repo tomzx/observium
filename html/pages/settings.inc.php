5c936397c8487e7da03f6941e6995abae6f76b60
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-08-29 16:41:12 +0000

    add info on top of global config page
    git-svn-id: http://www.observium.org/svn/observer/trunk@4390 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/settings.inc.php
+++ html/pages/settings.inc.php
@@ -2,6 +2,7 @@
 
 if ($_SESSION['userlevel'] == '10')
 {
+  print_warning("This is a dump of your Observium configuration. To adjust it, please modify your <strong>config.php</strong> file.");
   echo("<pre>");
   print_vars($config);
   echo("</pre>");
