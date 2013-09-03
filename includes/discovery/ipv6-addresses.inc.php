ec9fb469cc9280c3222f009c6341ef1ec867bcce
mike <mike@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-09-03 10:24:12 +0000

    eventlog ipv6 compressed.
    git-svn-id: http://www.observium.org/svn/observer/trunk@4432 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/ipv6-addresses.inc.php
+++ includes/discovery/ipv6-addresses.inc.php
@@ -123,6 +123,7 @@ if (count($ip_data))
       $full_address = $ipv6_address.'/'.$ipv6_prefixlen;
       $ipv6_network = Net_IPv6::getNetmask($full_address) . '/' . $ipv6_prefixlen;
       $ipv6_compressed = Net_IPv6::compress($ipv6_address);
+      $full_compressed = $ipv6_compressed.'/'.$ipv6_prefixlen;
       // First check networks
       $ipv6_network_id = dbFetchCell('SELECT `ipv6_network_id` FROM `ipv6_networks` WHERE `ipv6_network` = ?', array($ipv6_network));
       if (empty($ipv6_network_id))
@@ -143,11 +144,11 @@ if (count($ip_data))
           dbUpdate($update_array, 'ipv6_addresses', '`ipv6_address_id` = ?', array($old_table[$ifIndex][$ipv6_address]['ipv6_address_id']));
           if (isset($update_array['port_id']))
           {
-            log_event("IPv6 remove: $ipv6_address/".$old_table[$ifIndex][$ipv6_address]['ipv6_prefixlen'], $device, 'interface', $old_table[$ifIndex][$ipv6_address]['port_id']);
-            log_event("IPv6 add: $full_address", $device, 'interface', $port_id);
+            log_event("IPv6 remove: $ipv6_compressed/".$old_table[$ifIndex][$ipv6_address]['ipv6_prefixlen'], $device, 'interface', $old_table[$ifIndex][$ipv6_address]['port_id']);
+            log_event("IPv6 add: $full_compressed", $device, 'interface', $port_id);
           } elseif (isset($update_array['ipv6_prefixlen']))
           {
-            log_event("IPv6 change: $ipv6_address/".$old_table[$ifIndex][$ipv6_address]['ipv6_prefixlen']." -> $full_address", $device, 'interface', $port_id);
+            log_event("IPv6 change: $ipv6_compressed/".$old_table[$ifIndex][$ipv6_address]['ipv6_prefixlen']." -> $full_compressed", $device, 'interface', $port_id);
           }
           echo('U');
           $check_networks[$ipv6_network_id] = 1;
@@ -162,7 +163,7 @@ if (count($ip_data))
           $update_array[$param] = $$param;
         }
         dbInsert($update_array, 'ipv6_addresses');
-        log_event("IPv6 add: $full_address", $device, 'interface', $port_id);
+        log_event("IPv6 add: $full_compressed", $device, 'interface', $port_id);
         echo('+');
       }
       $valid_address = $full_address . '-' . $port_id;
@@ -181,7 +182,7 @@ foreach(dbFetchRows($query, array($device_id)) as $entry)
   {
     // Delete IP
     dbDelete('ipv6_addresses', '`ipv6_address_id` = ?', array($entry['ipv6_address_id']));
-    log_event("IPv6 remove: $full_address", $device, 'interface', $port_id);
+    log_event("IPv6 remove: $full_compressed", $device, 'interface', $port_id);
     echo('-');
     $check_networks[$entry['ipv6_network_id']] = 1;
   }
