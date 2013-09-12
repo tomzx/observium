599109473d83112de73933ad9725497066dbd8ad
sid3windr <sid3windr@61d68cd4-352d-0410-923a-c4978735b2b8>
2013-09-12 15:25:07 +0000

    clean up unnecessary sensor include files
    git-svn-id: http://www.observium.org/svn/observer/trunk@4486 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/sensors.inc.php
+++ includes/discovery/sensors.inc.php
@@ -10,21 +10,30 @@ include("includes/discovery/entity-sensor.inc.php");
 $include_dir = "includes/discovery/sensors";
 include("includes/include-dir.inc.php");
 
-echo("\n");
+$include_dir = "includes/discovery/dbm";
+include("includes/include-dir.inc.php");
 
-include("includes/discovery/dbm.inc.php");
-include("includes/discovery/temperatures.inc.php");
-include("includes/discovery/humidity.inc.php");
-include("includes/discovery/voltages.inc.php");
-include("includes/discovery/frequencies.inc.php");
-include("includes/discovery/current.inc.php");
-include("includes/discovery/power.inc.php");
+$include_dir = "includes/discovery/temperatures";
+include("includes/include-dir.inc.php");
 
-$sensortypes = array('fanspeed');
+$include_dir = "includes/discovery/humidity";
+include("includes/include-dir.inc.php");
 
-foreach ($sensortypes as $type)
+$include_dir = "includes/discovery/voltages";
+include("includes/include-dir.inc.php");
+
+$include_dir = "includes/discovery/frequencies";
+include("includes/include-dir.inc.php");
+
+$include_dir = "includes/discovery/current";
+include("includes/include-dir.inc.php");
+
+$include_dir = "includes/discovery/power";
+include("includes/include-dir.inc.php");
+
+foreach (array_keys($config['sensor_types']) as $type)
 {
-  if ($debug) { print_vars($valid['sensor']['$type']); }
+  if ($debug) { print_vars($valid['sensor'][$type]); }
   check_valid_sensors($device, $type, $valid['sensor']);
 }
 
